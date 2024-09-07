b5c13a2e28b8a717308c213c584f384d957020a5
KaterynaWalls <katerynawalls@proton.me>
2024-09-07 16:04:26 +0000

    Deploying to gh-pages from @ KaterynaWalls/goit-js-hw-09@ba2689189fd25af64f1d399e2615f01d52a7c205 🚀

--- 1-gallery.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-B4s-4mBC.js";console.log("Gellery");
-//# sourceMappingURL=1-gallery.js.map
--- 2-form.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-B4s-4mBC.js";console.log("Form");
-//# sourceMappingURL=2-form.js.map
